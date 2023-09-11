0bedc81b6ffad4d045a11148e9b023547eb3f4d3
davinalifianda123 <dav1nalifianda@gmail.com>
2023-09-11 21:05:30 +0700

    tind up sucsess

--- main.js
+++ main.js
@@ -13,3 +13,23 @@ function toggleDetail(e) {
 
     $(detail).slideToggle()
 }
+
+function onFormSubmit(e) {
+    e.preventDefault()
+    const email = $("#inp_email")
+    const subject = $("#inp_subject")
+    const massage = $("#inp_massage")
+
+    if(!$(email).val()) {
+        alert("email mu belum di isi")
+    } else if(!$(subject).val()) {
+        alert("subject mu di isi sek")
+    } else if(!$(massage).val()) {
+        alert("wes pokokmen isi kabeh")
+    } else {
+        alert("sucsess")
+        $(email).val("")
+        $(subject).val("")
+        $(massage).val("")
+    }
+}
